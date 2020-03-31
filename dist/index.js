878068122db86c15dc0ce19f3ead2a3f9f834a05
Wang "undoZen" Yu <opensource@undozen.com>
2020-03-31 15:02:26 +0800

    yarn build

--- dist/index.js
+++ dist/index.js
@@ -1,39 +1,49 @@
 'use strict';
 
-Object.defineProperty(exports, '__esModule', { value: true });
-
 function _interopDefault (ex) { return (ex && (typeof ex === 'object') && 'default' in ex) ? ex['default'] : ex; }
 
 var produce = _interopDefault(require('immer'));
 var rxjs = require('rxjs');
 var operators = require('rxjs/operators');
-var fastJsonPatch = require('fast-json-patch');
 var isPrimitive = _interopDefault(require('is-primitive'));
+var fastJsonPatch = require('fast-json-patch');
 
-function produceOperator(fn) {
-  return rxjs.pipe(
-    operators.scan(([last], current) => {
-      return [produce(last, draft => fn(draft, current))];
-    }, []),
-    operators.pluck(0),
-  );
-}
-function mapOperator(fn) {
-  return produceOperator((last, current) => {
-    current = fn(current);
-    if (isPrimitive(last) || isPrimitive(current)) {
-      return current;
-    }
-    return produce(last, draft => {
-      fastJsonPatch.applyPatch(draft, fastJsonPatch.compare(last, current));
+function newProduce(current, last, fn, skipCompare) {
+  return produce(last, draft => {
+    let patches;
+    let returned;
+    const result = produce(last, innerDraft => {
+      returned = fn(current, innerDraft);
+      if (returned === undefined) {
+        return;
+      }
+      if (!skipCompare) {
+        patches = fastJsonPatch.compare(last, returned);
+      }
     });
+    if (skipCompare) {
+      return result;
+    }
+    if (!patches) {
+      patches = fastJsonPatch.compare(last, result);
+    }
+    fastJsonPatch.applyPatch(draft, patches);
   });
 }
-function mapIdentity() {
-  return mapOperator(rxjs.identity);
+
+function produceOperator(fn, skipCompare) {
+  if (!fn) {
+    fn = rxjs.identity;
+  }
+  return rxjs.pipe(
+    operators.scan(([last], current) => {
+      if (isPrimitive(last) || isPrimitive(current)) {
+        return [produce(current, () => {})];
+      }
+      return [newProduce(current, last, fn, skipCompare)];
+    }, []),
+    operators.pluck(0)
+  );
 }
 
-exports.default = mapIdentity;
-exports.map = mapOperator;
-exports.mapIdentity = mapIdentity;
-exports.produce = produceOperator;
+module.exports = produceOperator;
